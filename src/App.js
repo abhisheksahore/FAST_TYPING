cb568a6acc421fc8a6fe38951ab835d59fe4f81b
Abhishek Sahore <abhisheksahore7@gmail.com>
2021-01-19 20:07:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from 'react';
 
 function App() {
+  const STARTING_TIME = 5;
+  const [text, setText] = useState('');
+  const [time, setTime] = useState(STARTING_TIME);
+  const [start, setStart] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
+
+  const inputRef = useRef(null);
+
+  function handleChange(e) {
+    const {value} = e.target;
+    setText(value);
+  }
+  console.log(text)
+
+  function calculateWordCount(text) {
+    const wordsArray = text.split(' ');
+    return wordsArray.filter(word => word !== '').length;
+  }
+  console.log(calculateWordCount(text))
+
+  const startGame = () => {
+    setStart(true);
+    setTime(STARTING_TIME);
+    setText('');
+    inputRef.current.disabled = false;
+    inputRef.current.focus();
+  }
+
+  const endGame = () => {
+    setStart(false)
+    setWordCount(calculateWordCount(text));
+  }
+
+  useEffect(()=> {
+    if(time > 0 && start){
+      setTimeout(()=> {
+        setTime(prevTime => (prevTime-1));
+      }, 1000);
+    } else if(time === 0) {
+      endGame();
+    }
+  }, [start, time])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>How fast do you type?</h1>
+      <textarea ref={inputRef} name='textarea' value={text} onChange={handleChange} disabled={!start}/>
+      <h4>time remaining: {time}</h4>
+      <button onClick={startGame} disabled={start}>start</button>
+      <h1>word count: {(time===0 && start===false)? wordCount: 0}</h1  >
     </div>
   );
 }
